eab31ad5e299b55b7d61ba276e167ef139eeac18
Elijah Jeruh <elijahgaskinsgeller@gmail.com>
2025-02-13 10:27:08 -0800

    detect if mobile device and raycast imput

--- js/page_1.js
+++ js/page_1.js
@@ -1,7 +1,8 @@
 
 
 import * as THREE from 'three';
-
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js'
+import { DeviceOrientationControls } from './DeviceOrientationControls.js';
 
 console.log(THREE);
 
@@ -20,10 +21,37 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
+let raycaster = new THREE.Raycaster();
+
+
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 let cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+
+let mobileMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+let desktopControls = new PointerLockControls(camera, renderer.domElement);
+let mobileControls = null;
+console.log(desktopControls);
+let confirmDevice = false;
+
+function OnDeviceTilt(e) {
+
+	console.log(e);
+
+	if (!confirmDevice && e.alpha !== null && e.beta !== null && e.gamma !== null) {
+		cube.material = mobileMaterial;
+		confirmDevice = true;
+
+		desktopControls.disconnect();
+		desktopControls.dispose();
+
+		mobileControls = new DeviceOrientationControls(camera);
+	}
+
+
+}
 
 
 function OnWindowResize(e) {
@@ -34,15 +62,71 @@ function OnWindowResize(e) {
 
 }
 
+function OnPointerDown(e) {
+
+	let pointer = new THREE.Vector2();
+	pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+	pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+
+	raycaster.setFromCamera(pointer, camera);
+	let intersects = raycaster.intersectObject(cube);
+
+	if (intersects.length > 0) {
+
+		console.log(intersects[0]);
+		intersects[0].object.material.color.set('#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'));
+
+	}
+
+}
+
+
+function OnMouseDown(e) {
+	desktopControls.lock();
+};
+
+function OnMouseUp(e) {
+	if (mobileControls === null) {
+		desktopControls.unlock();
+	}
+}
+
+function OnTouchStart(e) {
+	if (mobileControls === null) {
+		desktopControls.lock();
+	}
+}
+
+function OnTouchEnd(e) {
+	if (mobileControls === null) {
+		desktopControls.unlock();
+	}
+}
+
+
 
 function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+	if (mobileControls !== null) {
+
+		mobileControls.update();
+
+	}
+
 	renderer.render(scene, camera);
 }
 
 
 
 window.addEventListener("resize", OnWindowResize);
+window.addEventListener("deviceorientation", OnDeviceTilt);
+window.addEventListener("mousedown", OnMouseDown);
+window.addEventListener("mouseup", OnMouseUp);
+window.addEventListener("pointerdown", OnPointerDown);
+window.addEventListener("touchstart", OnTouchStart);
+window.addEventListener("touchend", OnTouchEnd);
 
 renderer.setAnimationLoop(animate);
