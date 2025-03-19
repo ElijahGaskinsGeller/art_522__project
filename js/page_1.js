29aa99b5d9de5902c5d6daf50f715b6c2a04797e
Elijah Jeruh <elijahgaskinsgeller@gmail.com>
2025-03-19 15:01:13 -0700

    adding chanels to p1

--- js/page_1.js
+++ js/page_1.js
@@ -20,29 +20,143 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let texturePrefix = "scene_";
-let environment = new THREE.CubeTextureLoader().setPath("./page_1/").load([
-	texturePrefix + "left.jpg",
-	texturePrefix + "right.jpg",
-	texturePrefix + "top.jpg",
-	texturePrefix + "bottom.jpg",
-	texturePrefix + "back.jpg",
-	texturePrefix + "front.jpg"
-], function(texture) {
+let texturePrefix = "./page_1/scene_";
+let cubeTextures = [];
+let currentChanel = 0;
 
-	renderer.initTexture(texture);
-	scene.background = texture;
+function SetBackground() {
 
+
+
+	console.log("here");
+
+	cubeTextures[0] = new THREE.CubeTexture([
+
+		leftTexture_0,
+		rightTexture,
+		topTexture,
+		bottomTexture,
+		backTexture,
+		frontTexture
+
+	]);
+
+	cubeTextures[1] = new THREE.CubeTexture([
+
+		leftTexture_1,
+		rightTexture,
+		topTexture,
+		bottomTexture,
+		backTexture,
+		frontTexture
+
+	]);
+
+	for (let i = 0; i < cubeTextures.length; i++) {
+		cubeTextures[i].needsUpdate = true;
+		cubeTextures[i].colorSpace = THREE.SRGBColorSpace;
+		renderer.initTexture(cubeTextures[i]);
+	}
+	scene.background = cubeTextures[0];
+
+	console.log(scene.background);
+
+}
+
+
+let imageLoader = new THREE.ImageLoader();
+
+let loadTarget = 7;
+let loaded = 0;
+
+let frontTexture = imageLoader.load(texturePrefix + "front.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
 });
+
+
+let backTexture = imageLoader.load(texturePrefix + "back.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+let leftTexture_0 = imageLoader.load(texturePrefix + "left_0.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+let leftTexture_1 = imageLoader.load(texturePrefix + "left_1.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+let rightTexture = imageLoader.load(texturePrefix + "right.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+let topTexture = imageLoader.load(texturePrefix + "top.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+let bottomTexture = imageLoader.load(texturePrefix + "bottom.jpg", function(image) {
+	loaded++;
+	if (loaded == loadTarget) {
+		SetBackground();
+	}
+});
+
+
+//let cubeTexture = new THREE.CubeTexture([
+//
+//	leftTexture,
+//	rightTexture,
+//	topTexture,
+//	bottomTexture,
+//	backTexture,
+//	frontTexture
+//
+//]);
+//scene.background = cubeTexture;
+//
+//let environment = new THREE.CubeTextureLoader().setPath("./page_1/").load([
+//	texturePrefix + "left_0.jpg",
+//	texturePrefix + "right.jpg",
+//	texturePrefix + "top.jpg",
+//	texturePrefix + "bottom.jpg",
+//	texturePrefix + "back.jpg",
+//	texturePrefix + "front.jpg"
+//], function(texture) {
+//
+//	console.log(texture);
+//renderer.initTexture(texture);
+//	scene.background = texture;
+//
+//});
 
 
 let raycaster = new THREE.Raycaster();
 
 
-let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let geometry = new THREE.BoxGeometry(1.3, 1.3, 1.3);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: .0 });
 let cube = new THREE.Mesh(geometry, material);
-//scene.add(cube);
+scene.add(cube);
+cube.position.x = -3;
+cube.position.z = 4.7;
 
 
 let mobileMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
@@ -80,19 +194,30 @@ function OnWindowResize(e) {
 function OnPointerDown(e) {
 
 	let pointer = new THREE.Vector2();
-	pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-	pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+	pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
+	pointer.y = - (e.clientY / window.innerHeight) * 2 + 1;
 
 
 	raycaster.setFromCamera(pointer, camera);
-	//let intersects = raycaster.intersectObject(cube);
+	let intersects = raycaster.intersectObject(cube);
 
-	//if (intersects.length > 0) {
+	if (intersects.length > 0) {
 
-	//console.log(intersects[0]);
-	//intersects[0].object.material.color.set('#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'));
+		console.log(intersects[0]);
 
-	//}
+		if (cubeTextures.length > 0) {
+
+			console.log("here");
+
+			currentChanel++;
+			if (currentChanel >= cubeTextures.length) currentChanel = 0;
+			scene.background = cubeTextures[currentChanel];
+
+
+		}
+		//intersects[0].object.material.color.set('#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'));
+
+	}
 
 }
 
